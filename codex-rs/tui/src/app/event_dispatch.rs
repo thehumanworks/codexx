yukimaru77/codex

Use model service tier slash commands
Expose model service tier choices as slash commands and route selections through service_tier_id so custom tier ids remain preserved.

Co-authored-by: Codex <noreply@openai.com>

codex-rs/tui/src/app/event_dispatch.rs
@@ -1253,31 +1253,32 @@ impl App {
                     }
                 }
             }
-            AppEvent::PersistServiceTierSelection { service_tier } => {
+            AppEvent::PersistServiceTierSelection {
+                service_tier,
+                service_tier_id,
+            } => {
                 self.refresh_status_line();
                 let profile = self.active_profile.as_deref();
                 self.config.service_tier = service_tier;
-                self.config.service_tier_id =
-                    service_tier.map(|service_tier| service_tier.request_value().to_string());
+                self.config.service_tier_id = service_tier_id.or_else(|| {
+                    service_tier.map(|service_tier| service_tier.request_value().to_string())
+                });
                 let mut edits = ConfigEditsBuilder::new(&self.config.codex_home)
                     .with_profile(profile)
                     .set_service_tier(service_tier)
                     .set_service_tier_id(self.config.service_tier_id.clone());
-                if service_tier.is_none() {
+                if self.config.service_tier_id.is_none() {
                     self.config.notices.fast_default_opt_out = Some(true);
                     edits = edits.set_fast_default_opt_out(/*opted_out*/ true);
                 }
                 match edits.apply().await {
                     Ok(()) => {
-                        let status = if matches!(
-                            service_tier,
-                            Some(codex_protocol::config_types::ServiceTier::Fast)
-                        ) {
-                            "on"
-                        } else {
-                            "off"
-                        };
-                        let mut message = format!("Fast mode set to {status}");
+                        let mut message =
+                            if let Some(service_tier_id) = &self.config.service_tier_id {
+                                format!("Service tier set to {service_tier_id}")
+                            } else {
+                                "Service tier cleared".to_string()
+                            };
                         if let Some(profile) = profile {
                             message.push_str(" for ");
                             message.push_str(profile);
@@ -1286,14 +1287,14 @@ impl App {
                         self.chat_widget.add_info_message(message, /*hint*/ None);
                     }
                     Err(err) => {
-                        tracing::error!(error = %err, "failed to persist fast mode selection");
+                        tracing::error!(error = %err, "failed to persist service tier selection");
                         if let Some(profile) = profile {
                             self.chat_widget.add_error_message(format!(
-                                "Failed to save Fast mode for profile `{profile}`: {err}"
+                                "Failed to save service tier for profile `{profile}`: {err}"
                             ));
                         } else {
                             self.chat_widget.add_error_message(format!(
-                                "Failed to save default Fast mode: {err}"
+                                "Failed to save default service tier: {err}"
                             ));
                         }
                     }

thehumanworks/codexx

codex-rs/tui/src/app/event_dispatch.rs
@@ -1271,29 +1271,47 @@ impl App {
                     }
                 }
             }
-            AppEvent::PersistServiceTierSelection { service_tier } => {
+            AppEvent::PersistServiceTierSelection {
+                service_tier,
+                service_tier_id,
+            } => {
                 self.refresh_status_line();
                 let profile = self.active_profile.as_deref();
-                self.config.service_tier =
-                    service_tier.map(|service_tier| service_tier.request_value().to_string());
+                self.config.service_tier = service_tier_id.or_else(|| {
+                    service_tier.map(|service_tier| service_tier.request_value().to_string())
+                });
+                let service_tier_segments = if let Some(profile) = profile {
+                    vec![
+                        "profiles".to_string(),
+                        profile.to_string(),
+                        "service_tier".to_string(),
+                    ]
+                } else {
+                    vec!["service_tier".to_string()]
+                };
+                let service_tier_edit = match &self.config.service_tier {
+                    Some(service_tier) => ConfigEdit::SetPath {
+                        segments: service_tier_segments,
+                        value: service_tier.clone().into(),
+                    },
+                    None => ConfigEdit::ClearPath {
+                        segments: service_tier_segments,
+                    },
+                };
                 let mut edits = ConfigEditsBuilder::new(&self.config.codex_home)
-                    .with_profile(profile)
-                    .set_service_tier(service_tier);
-                if service_tier.is_none() {
+                    .with_edits([service_tier_edit]);
+                if self.config.service_tier.is_none() {
                     self.config.notices.fast_default_opt_out = Some(true);
                     edits = edits.set_fast_default_opt_out(/*opted_out*/ true);
                 }
                 match edits.apply().await {
                     Ok(()) => {
-                        let status = if matches!(
-                            service_tier,
-                            Some(codex_protocol::config_types::ServiceTier::Fast)
-                        ) {
-                            "on"
-                        } else {
-                            "off"
-                        };
-                        let mut message = format!("Fast mode set to {status}");
+                        let mut message =
+                            if let Some(service_tier_id) = &self.config.service_tier {
+                                format!("Service tier set to {service_tier_id}")
+                            } else {
+                                "Service tier cleared".to_string()
+                            };
                         if let Some(profile) = profile {
                             message.push_str(" for ");
                             message.push_str(profile);
@@ -1302,14 +1320,14 @@ impl App {
                         self.chat_widget.add_info_message(message, /*hint*/ None);
                     }
                     Err(err) => {
-                        tracing::error!(error = %err, "failed to persist fast mode selection");
+                        tracing::error!(error = %err, "failed to persist service tier selection");
                         if let Some(profile) = profile {
                             self.chat_widget.add_error_message(format!(
-                                "Failed to save Fast mode for profile `{profile}`: {err}"
+                                "Failed to save service tier for profile `{profile}`: {err}"
                             ));
                         } else {
                             self.chat_widget.add_error_message(format!(
-                                "Failed to save default Fast mode: {err}"
+                                "Failed to save default service tier: {err}"
                             ));
                         }
                     }